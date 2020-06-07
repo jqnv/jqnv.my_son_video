33a3bbe772a9e3b495cc34580d267c492fef1281
jqnv <jqnv@users.noreply.github.com>
2020-06-06 20:34:49 -0600

    ssd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var name="audacity";
-var newname=name.replace(name.slice(0, 2),"U");
+var skills=["awesomeness","programming","teaching","JS"];
 
-//$("#header").append(formattedName, formattedRole);
-console.log(newname);
-//$("#header").prepend(formattedName);
+$("#main").append(skills);
