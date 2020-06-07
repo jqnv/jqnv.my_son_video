ef5359cca083e04c886d603a1a3606747648f53c
jqnv <jqnv@users.noreply.github.com>
2020-06-06 20:16:41 -0600

    nf

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,7 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 var name="audacity";
-var newname=name.replace(name.slice(0, 1));
+var newname=name.replace(name.slice(0, 1),"U");
 
 //$("#header").append(formattedName, formattedRole);
 $("#header").prepend(newname);
