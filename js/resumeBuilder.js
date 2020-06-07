07dfdd4da39f8bb9a8848ef125bb55d7f7b793e4
jqnv <jqnv@users.noreply.github.com>
2020-06-06 18:15:59 -0600

    dh

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name="Arturo";
-var age =43;
 
-console.log(name);
+var name="Arturo";
+var awesomeThoughts ="I am {{name}} and I am AWESOME";
+
+console.log(awesomeThoughts);
