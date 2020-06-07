d5480da5a01a003c6c41ce2616a1a1b7911b9348
jqnv <jqnv@users.noreply.github.com>
2020-06-06 19:34:19 -0600

    gd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,6 +4,6 @@ This is empty on purpose! Your code to build the resume will go here.
 
 var name="Joaquín Vásquez";
 
-var formattedName=HTMLheaderName.replace(%data%, name);
+var formattedName=HTMLheaderName.replace("%data%", name);
 
 $("#header").append(formattedName);
